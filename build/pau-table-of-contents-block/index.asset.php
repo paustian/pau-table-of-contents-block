3d964f3b38df2349b108ada92ffe48dd686d6137
Timothy Paustian <tdpaustian@gmail.com>
2025-10-25 10:32:59 -0500

    Font size for editor/front end
    Fixed the font size text display for editor and front end

--- build/pau-table-of-contents-block/index.asset.php
+++ build/pau-table-of-contents-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'da767814a67c5858037f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8f0f9f7327191f31ffac');
